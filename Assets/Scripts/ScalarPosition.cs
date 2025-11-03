6bee724f708c3621699a00a923906b30562c7dec
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-03 12:49:10 -0400

    added scene 3
    looked at description of code  required  at a total loss will need to read more later

--- Assets/Scripts/ScalarPosition.cs
+++ Assets/Scripts/ScalarPosition.cs
@@ -1,35 +1,35 @@
-//using System;
-//using UnityEngine;
+using System;
+using UnityEngine;
 
-//public class ScalarPosition : MonoBehaviour
-//{
+public class ScalarPosition : MonoBehaviour
+{
 
-//    [SerializeField] float step;
-//    static int scalar = 1;
-
-
-
-
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Start()
-//    {
-//        Vector3 startPos = new Vector3(0, 0, 0); 
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-    
-
-//        if (!Console.KeyAvailable) return;
-
-//        ConsoleKeyInfo inputKey = Console.ReadKey(true);
-
-//        if (inputKey.Key == ConsoleKey.S)  startPos = startPos * scalar -= 1;
-
-//        if (inputKey.Key == ConsoleKey.W) startPos = startPos + scalar += 1;
+    [SerializeField] float step;
+    static int scalar = 1;
 
 
 
-//    }
-//}
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector3 startPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!Console.KeyAvailable) return;
+
+        ConsoleKeyInfo inputKey = Console.ReadKey(true);
+
+        if (inputKey.Key == ConsoleKey.S) scalar -= 1;
+
+        if (inputKey.Key == ConsoleKey.W) scalar += 1;
+
+
+
+    }
+}
